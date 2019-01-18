982207ff3babbc3557eaff1addcc83d2cdbf707f
Rennan de Almeida <dealmeida.rennan@gmail.com>
2019-01-18 12:23:48 -0200

    add codigo no arquivo
    modelo de pipeline para fazer deploy utilizando a tecnologia nodejs.
    Notificação (de sucesso ou falha) via Slack é acionada pos-pipeline.
    
    PS: Esse método foi aplicado em container utilizando uma imagem jenkins ( fonte:https://hub.docker.com/r/jenkins/jenkins/), caso esteja utilizando esse metodo é importante montar o volume para que o deploy funcione.

---
+++ pipeline_npm_sentSlack.js
@@ -0,0 +1,41 @@
+pipeline {
+  agent any
+  stages {
+    stage('install package') {
+      steps {
+        nodejs('node') {
+          sh 'npm install'
+        }
+
+      }
+    }
+    stage('run build') {
+      steps {
+        nodejs('node') {
+          sh 'npm run build'
+        }
+
+      }
+    }
+    stage('deploy') {
+      steps {
+        sh '''cp -r $JENKINS_HOME/workspace/build_package $JENKINS_HOME/workspace/volume-montado/build_package_deploy
+
+rm -rf $JENKINS_HOME/workspace/volume-montado/package_em_prod
+
+mv $JENKINS_HOME/workspace/volume-montado/build_package_deploy $JENKINS_HOME/workspace/volume-montado/package_em_prod'''
+      }
+    }
+  }
+  post {
+    success {
+      slackSend(baseUrl: 'https://SeuWorkSpaceDoSlack.slack.com/services/hooks/jenkins-ci/', token: 'xxxxxxxxxxxxx', channel: 'CanalSlack', message: 'DEPLOY SUCCESS!')
+
+    }
+
+    failure {
+      slackSend(baseUrl: 'https://SeuWorkSpaceDoSlack.slack.com/services/hooks/jenkins-ci/', token: 'xxxxxxxxxxxxx', channel: 'CanalSlack', message: 'DEPLOY FAILURE')
+
+    }
+}
+}
